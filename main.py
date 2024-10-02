0e67e0a383716f4f7c7c88643e94fbbdbf29b6e9
sm7177 <subrinamaharjan2020@gmail.com>
2024-10-02 15:29:57 +0545

    .

---
+++ main.py
@@ -0,0 +1,8 @@
+class Robot:
+    r1="Tom"
+    r2="Jerry"
+    def intro(self):
+        print("I am robot-1 and my nane is",self.r1)
+        print("I am robot-2 and my name is",self.r2)
+obj=Robot()
+obj.intro()
